d93fb916ed139780d99771dfc342956bb425e836
Priyanshu Dash <priynashudashofficial@gmail.com>
2023-10-20 00:27:47 +0530

    Starting with the main design of dashboard

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,47 +1,110 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard.jsx'
-import Documentries from './components/Documentries.jsx'
-import Book from './components/Book.jsx'
-import ResearchPaper from './components/ResearchPaper.jsx'
-import ShortFilm from './components/ShortFilm.jsx'
-import Settings from './components/Settings.jsx'
-import { Link } from 'react-router-dom';
-
-
 function App() {
-
   return (
     <>
-    <Router>
-      <div>
-        <Link to='/' > DashBoard </Link>
-        <Link to='/book' > Books </Link>
-        <Link to='/documentries' > Documentries </Link>
-        <Link to='/short-film' > ShortFilms </Link>
-        <Link to='/research-paper' > Reasearcg Papers </Link>
-        <Link to='/settings' > Settings </Link>
-        <Link to='/library' > Libarary </Link>
-      </div>
-      <Switch>
-        <Route path='/' exact component={ Dashboard } />
-
-        <Route path='/documentries' Component={ Documentries } />
-
-        <Route path='/book' Component={ Book } />
-
-        <Route path='/short-film' Component={ ShortFilm } />
-
-        <Route path='/research-paper' Component={ ResearchPaper } />
-
-        <Route path='/settings' Component={ Settings } />
-
-        <Route path='/library' Component={ Library } />
-
-      </Switch>
-    </Router>
-      
     </>
-  )
+      
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard.jsx';
+import Documentries from './components/Documentries.jsx';
+import Book from './components/Book.jsx';
+import ResearchPaper from './components/ResearchPaper.jsx';
+import ShortFilm from './components/ShortFilm.jsx';
+import Settings from './components/Settings.jsx';
+import { Link } from 'react-router-dom';
+import Library from './components/Library.jsx';
+
+
+
+<Router>
+        <div>
+          <Link to="/">Dashboard</Link>
+          <Link to="/book">Books</Link>
+          <Link to="/documentaries">Documentaries</Link>
+          <Link to="/short-film">Short Films</Link>
+          <Link to="/research-paper">Research Papers</Link>
+          <Link to="/settings">Settings</Link>
+          <Link to="/library">Library</Link>
+        </div>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/documentaries" component={Documentries} />
+          <Route path="/book" component={Book} />
+          <Route path="/short-film" component={ShortFilm} />
+          <Route path="/research-paper" component={ResearchPaper} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/library" component={Library} />
+        </Switch>
+      </Router>
+    </>
+    */
